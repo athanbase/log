61aa2e186727592f92d22a1e7c552686ab182648
athanbase <athanbase@proton.me>
2022-10-22 21:14:32 +0800

    feat: wrap zap

---
+++ log.go
@@ -0,0 +1,112 @@
+package log
+
+import (
+	"io"
+	"os"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+type Level = zapcore.Level
+
+const (
+	InfoLevel   Level = zap.InfoLevel   // 0, default level
+	WarnLevel   Level = zap.WarnLevel   // 1
+	ErrorLevel  Level = zap.ErrorLevel  // 2
+	DPanicLevel Level = zap.DPanicLevel // 3, used in development log
+	// PanicLevel logs a message, then panics
+	PanicLevel Level = zap.PanicLevel // 4
+	// FatalLevel logs a message, then calls os.Exit(1).
+	FatalLevel Level = zap.FatalLevel // 5
+	DebugLevel Level = zap.DebugLevel // -1
+)
+
+type Field = zap.Field
+
+type Logger struct {
+	l     *zap.Logger // zap ensure that zap.Logger is safe for concurrent use
+	level Level
+}
+
+func (l *Logger) Debug(msg string, fields ...Field) {
+	l.l.Debug(msg, fields...)
+}
+
+func (l *Logger) Info(msg string, fields ...Field) {
+	l.l.Info(msg, fields...)
+}
+
+func (l *Logger) Warn(msg string, fields ...Field) {
+	l.l.Warn(msg, fields...)
+}
+
+func (l *Logger) Error(msg string, fields ...Field) {
+	l.l.Error(msg, fields...)
+}
+func (l *Logger) DPanic(msg string, fields ...Field) {
+	l.l.DPanic(msg, fields...)
+}
+func (l *Logger) Panic(msg string, fields ...Field) {
+	l.l.Panic(msg, fields...)
+}
+func (l *Logger) Fatal(msg string, fields ...Field) {
+	l.l.Fatal(msg, fields...)
+}
+
+var (
+	std = New(os.Stderr, InfoLevel)
+
+	Info   = std.Info
+	Warn   = std.Warn
+	Error  = std.Error
+	DPanic = std.DPanic
+	Panic  = std.Panic
+	Fatal  = std.Fatal
+	Debug  = std.Debug
+)
+
+// not safe for concurrent use, replace default std
+func ResetDefault(l *Logger) {
+	std = l
+	Info = std.Info
+	Warn = std.Warn
+	Error = std.Error
+	DPanic = std.DPanic
+	Panic = std.Panic
+	Fatal = std.Fatal
+	Debug = std.Debug
+}
+
+func Default() *Logger { return std }
+
+// New create a new logger
+func New(w io.Writer, level Level) *Logger {
+	if w == nil {
+		panic("the writer is nil")
+	}
+
+	cfg := zap.NewProductionConfig()
+	core := zapcore.NewCore(
+		zapcore.NewJSONEncoder(cfg.EncoderConfig),
+		zapcore.AddSync(w),
+		zapcore.Level(level),
+	)
+
+	return &Logger{
+		l:     zap.New(core),
+		level: level,
+	}
+}
+
+func (l *Logger) Sync() error {
+	return l.l.Sync()
+}
+
+func Sync() error {
+	if std != nil {
+		return std.Sync()
+	}
+
+	return nil
+}
